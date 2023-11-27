314e5c737c5928874d67fdddd503463e37d7bcc8
DimitarDavidov <dimitardavidov5@gmail.com>
2023-11-27 17:45:52 +0200

    Details page is almost complete
    Edit, Delete and Like buttons are still in progress. Probably will make some changes on the css

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,56 +1,75 @@
 import { useParams } from 'react-router';
 import styles from './styles.module.css';
 import { useEffect, useState } from 'react';
-import {doc} from 'firebase/firestore'
+import { doc } from 'firebase/firestore'
 import firebase from '../../firebase';
 
 export const MovieDetails = () => {
 
-const {movieId} = useParams()
-const [movie, setMovie] = useState(null);
+    const { movieId } = useParams()
+    const [movie, setMovie] = useState(null);
 
-useEffect(() => {
-    const getMovie = async () => {
-        try {
-            const movieInfo = await firebase.firestore().collection('movies').doc(movieId).get();
-            setMovie(movieInfo.data())
-        }catch (error) {
-            console.log(error)
+    useEffect(() => {
+        const getMovie = async () => {
+            try {
+                const movieInfo = await firebase.firestore().collection('movies').doc(movieId).get();
+                setMovie(movieInfo.data())
+            } catch (error) {
+                console.log(error)
+            }
         }
+
+        getMovie()
+    }, [movieId]);
+
+    
+
+    if (!movie) {
+        return <div className={styles.LoadingDiv}>
+            <img src="./images/m.jpg" alt="logo" width="250" height="250" />
+        </div>
     }
 
-    getMovie()
-}, [movieId])
+    const getYouTubeVideoId = (url) => {
+        const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+        const match = url.match(regex);
+        return match;
+      };
 
-if (!movie) {
-    return <div className={styles.LoadingDiv}>
-    <img src="./images/m.jpg" alt="logo" width="250" height="250"/>
-    </div>
-  }
+      const youtubeVideoId = getYouTubeVideoId(movie.trailerUrl);
+    //   console.log(youtubeVideoId[1])
 
-return (
-    <div className={styles.DetailsDiv}>
-        <section className={styles.DetailsSection}>
-            <div className={styles.DetailsImage}>
-            <img src={movie.imageUrl} alt="movie" height={350} width={260} />
-            </div>
-            <div className={styles.DetailsInfo}>
-            <h5>Title: {movie.title}</h5>
-            <h5>Category: {movie.category}</h5>
-            <h5>Type: {movie.type}</h5>
-            <h5>Studio: {movie.studio}</h5>
-            <h5>Year: {movie.year}</h5>
-            </div>
-            <div className={styles.DetailsDescription}>
-                <p>{movie.description}</p>
-            </div>
-            <div className={styles.DetailsButtons}>
-            <button>Edit</button>
-            <button>Delete</button>
-            </div>
-        </section>
-    </div>
-)
+    return (
+        <div className={styles.DetailsDiv}>
+            <section className={styles.DetailsSection}>
+                <div>
+                    <img src={movie.imageUrl} alt="movie" height={350} width={260} />
+                    <div className={styles.DetailsInfo}>
+                        <h5>Title: <span>{movie.title}</span></h5>
+                        <h5>Category: <span>{movie.category}</span></h5>
+                        <h5>Type: <span>{movie.type}</span></h5>
+                        <h5>Studio: <span>{movie.creator}</span></h5>
+                        <h5>Year: <span>{movie.year}</span></h5>
+                        <h5>Likes: </h5>
+                        <h5><button>Like</button></h5>
+                    </div>
+                </div>
+                
+                <div className={styles.DetailsDescription}>
+                    <p>{movie.description}</p>
+                    <div className={styles.DetailsButtons}>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                </div>
+                <div>
+                    <iframe width="400" height="345" frameBorder={0} src={`https://www.youtube.com/embed/${youtubeVideoId[1]}`}></iframe>
+                </div>
+
+
+            </section>
+        </div>
+    )
 
 
 }
